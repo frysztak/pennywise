50a50e97de8f7b4795f618bf0cf635469059f3fd
Sebastian Frysztak <sebastian@frysztak.dev>
2026-01-23 17:33:10 +0100

    feat(frontend): hide breadcrumbs

--- web/src/routes/_pathlessLayout/route.tsx
+++ web/src/routes/_pathlessLayout/route.tsx
@@ -1,8 +1,6 @@
-import { Separator } from "@radix-ui/react-separator";
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from "@/components/ui/breadcrumb";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export const Route = createFileRoute("/_pathlessLayout")({
@@ -24,14 +22,14 @@ function RouteComponent() {
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            {/* <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem>
                   <BreadcrumbPage>Dashboard</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
-            </Breadcrumb>
+            </Breadcrumb> */}
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
