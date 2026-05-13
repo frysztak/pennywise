9a1b77b170ce28cd51331cfce3758b6d613cbf82
Sebastian Frysztak <217806+frysztak@users.noreply.github.com>
2026-05-13 16:52:32 +0200

    fix(fe): adjust alignment of Settings page

--- web/src/routes/_pathlessLayout/settings.tsx
+++ web/src/routes/_pathlessLayout/settings.tsx
@@ -18,10 +18,8 @@ function RouteComponent() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-semibold">Settings</h1>
-      </div>
+    <div className="flex flex-1 flex-col gap-4">
+      <h1 className="text-3xl font-bold tracking-tight">Settings</h1>
 
       <Card className="max-w-xl">
         <CardHeader>
