c19905f847af51feff0e80ad7fb155a36f87a4c2
Sebastian Frysztak <217806+frysztak@users.noreply.github.com>
2026-06-13 10:57:38 +0200

    fix(fe): use error translation pipeline on login page

--- web/src/routes/auth/login.tsx
+++ web/src/routes/auth/login.tsx
@@ -1,10 +1,10 @@
 import { useMutation } from "@connectrpc/connect-query";
 import { Navigate, createFileRoute } from "@tanstack/react-router";
-import { toast } from "sonner";
 
 import { useAuth } from "@/features/auth/auth";
 import { LoginForm } from "@/features/auth/components/login-form";
 import { loginWithPassword } from "@/gen/api/v1/auth-AuthService_connectquery";
+import { handleError } from "@/shared/lib/utils";
 
 export const Route = createFileRoute("/auth/login")({
   component: RouteComponent,
@@ -20,9 +20,7 @@ function RouteComponent() {
     onSuccess: (data) => {
       setUserData({ ...data, $typeName: "api.v1.UserInfoResponse" });
     },
-    onError: (error) => {
-      toast.error(error.rawMessage);
-    },
+    onError: handleError,
   });
 
   if (isAuthenticated) {
